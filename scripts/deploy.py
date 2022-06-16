94336c030b2fe0f9dd07e8dce053d25343ae0938
jccimino1967 <jccimino@gmail.com>
2022-06-16 19:38:47 -0300

    primer commit

---
+++ scripts/deploy.py
@@ -0,0 +1,33 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"La red es  {network.show_active()} ")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+
+    print(f"Contrato desplegado a {fund_me.address}")
+
+    print("termino")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
